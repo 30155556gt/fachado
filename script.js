20d2706dc0d4724a4fd48d8a4e363fa5f8810d85
Leonardotorre18 <Leonardotorre14@gmail.com>
2024-09-04 12:09:20 -0400

    continuacion

---
+++ script.js
@@ -0,0 +1,30 @@
+const datostring = "trabajo"
+
+const datoNumerico = 7
+
+
+// Make a request for a user with a given ID
+axios.get('https://leonardoapi.onrender.com/songs')
+    .then(function (response) {
+        // handle success
+        console.log(response);
+
+        response.data.songs.map((songs) => {
+
+            const container = document.getElementById('track-list')
+
+            const div = document.createElement('div')
+                getElementById('track-list)
+
+            )
+
+        })
+
+
+
+
+    }
+    )
+    .
+
+    constlog(datoString) 
